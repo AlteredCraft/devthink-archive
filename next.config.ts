d05d17ebba10797d8f231925627bd65053b14a54
Sam Keen <sam.sjk@gmail.com>
2025-08-24 10:31:45 -0700

    adapting to new domain devthink.alteredcraft.com

--- next.config.ts
+++ next.config.ts
@@ -6,8 +6,6 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/devthink-archive' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/devthink-archive/' : '',
 };
 
 export default nextConfig;
